d5859feb40b908381334ba9ae19c51470265fe34
Blake Marley <bmarley@credera.com>
2014-06-27 18:06:06 -0500

    Don't need todays results in order to render the intitial page

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,16 @@
 var db = require('../utils/mongoWrapper');
 
 exports.index = function(req, res){
-    var request = require('request');
-    var mongoDb = db.getDbInstance();
-    mongoDb.world_cup_data.find({key: 'group_results'}, function(err, group_results){
+  var request = require('request');
+  var mongoDb = db.getDbInstance();
+  mongoDb.world_cup_data.find({key: 'group_results'}, function(err, group_results){
+    if ( err || !group_results) console.log('Likely no connection');
+    else mongoDb.world_cup_data.find({key: 'matches'}, function(err, matches) {
       if ( err || !group_results) console.log('Likely no connection');
-      else mongoDb.world_cup_data.find({key: 'matches'}, function(err, matches) {
-        if ( err || !group_results) console.log('Likely no connection');
-        else mongoDb.world_cup_data.find({key: 'today'}, function(err, today) {
-          if ( err || !group_results) console.log('Likely no connection');
-          else res.render('index', {
-            group_results: group_results[0].data,
-            matches: matches[0].data,
-            today: today[0].data});
-        })
-      })
-    })
+      else res.render('index', {
+          group_results: group_results[0].data,
+          matches: matches[0].data }
+      );
+    });
+  });
 };
