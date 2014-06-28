92a12d77d07ef39360e3beec81a3e2d23804d7de
Blake Marley <bmarley@credera.com>
2014-06-27 19:51:56 -0500

    Cleaning up the polling code

--- utils/mongoWrapper.js
+++ utils/mongoWrapper.js
@@ -5,6 +5,17 @@ var db = null
 //Start the db with mongod --dbpath "make a folder somewhere and put the path here"
 
 
+var requestAndUpdate = function (url, key) {
+  request(url, function (error, response, data) {
+    if (!error && response.statusCode == 200) {
+      db.world_cup_data.update({key: key}, {$set: {data: data}}, {upsert: true}, function (err, saved) {
+        if (err || !saved) console.log(key + ' errored');
+        else console.log('Boom');
+      })
+    }
+  });
+}
+
 module.exports = {
   initialize: function(next){
     this.initializeDB(next);
@@ -13,35 +24,14 @@ module.exports = {
     var databaseUrl = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost:27017'
     var collections = ["world_cup_data"];
     db = require("mongojs").connect(databaseUrl, collections);
-    this.startPolling(db);
+    this.startPolling();
     next();
   },
-  startPolling: function(db) {
+  startPolling: function() {
     var setPoll = setInterval(function() {
-      request('http://worldcup.sfg.io/group_results', function (error, response, group_results) {
-        if (!error && response.statusCode == 200) {
-          db.world_cup_data.update({key: 'group_results'}, {$set: {data: group_results}}, {upsert: true}, function (err, saved) {
-            if (err || !saved) console.log('groups errored');
-            else console.log('Boom');
-          })
-        }
-      });
-      request('http://worldcup.sfg.io/matches', function (error, response, matches) {
-        if (!error && response.statusCode == 200) {
-          db.world_cup_data.update({key: 'matches'}, {$set: {data: matches}}, {upsert: true}, function (err, saved) {
-            if (err || !saved) console.log('matches errored');
-            else console.log('Boom');
-          })
-        }
-      });
-      request('http://worldcup.sfg.io/matches/today', function (error, response, today) {
-        if (!error && response.statusCode == 200) {
-          db.world_cup_data.update({key: 'today'}, {$set: {data: today}}, {upsert: true}, function (err, saved) {
-            if (err || !saved) console.log('today errored');
-            else console.log('Boom');
-          });
-        }
-      });
+      requestAndUpdate('http://worldcup.sfg.io/group_results', 'group_results');
+      requestAndUpdate('http://worldcup.sfg.io/matches', 'matches');
+      requestAndUpdate('http://worldcup.sfg.io/matches/today', 'today');
     }, 200000);
   },
   getDbInstance: function() {
