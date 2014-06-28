834159730558666b1f16adf96ea0e0853d258647
Blake Marley <bmarley@credera.com>
2014-06-27 19:29:04 -0500

    Cleaning index.js a tad

--- routes/index.js
+++ routes/index.js
@@ -5,55 +5,47 @@
 
 var db = require('../utils/mongoWrapper');
 
-exports.index = function(req, res){
+// Cleaning -  key = key string in mdb, cb = callback when finished
+var getSomething = function(key, cb){
   var mongoDb = db.getDbInstance();
-  mongoDb.world_cup_data.find({key: 'group_results'}, function(err, group_results){
-    if ( err || !group_results) console.log('Likely no connection');
-    else mongoDb.world_cup_data.find({key: 'matches'}, function(err, matches) {
-      if ( err || !group_results) console.log('Likely no connection');
-      else res.render('index', {
-          group_results: group_results[0].data,
-          matches: matches[0].data }
-      );
+  mongoDb.world_cup_data.find({key: key}, function(err, data){
+    if ( err || !data) {
+      console.log('Likely no connection');
+    }
+    else {
+      cb(data);
+    }
+  })
+};
+
+exports.index = function(req, res){
+  getSomething('group_results', function(group_results){
+    getSomething('matches', function(matches){
+      res.render('index', {
+        group_results: group_results[0].data,
+        matches: matches[0].data
+      });
     });
   });
 };
 
 exports.group_results = function(req, res){
-  var mongoDb = db.getDbInstance();
-  mongoDb.world_cup_data.find({key: 'group_results'}, function(err, group_results){
-    if ( err || !group_results) {
-      console.log('Likely no connection');
-    }
-    else {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(group_results[0].data);
-    }
-  })
+  getSomething('group_results', function (data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(data[0].data);
+  });
 };
 
 exports.matches = function(req, res){
-  var mongoDb = db.getDbInstance();
-  mongoDb.world_cup_data.find({key: 'matches'}, function(err, matches){
-    if ( err || !matches) {
-      console.log('Likely no connection');
-    }
-    else {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(matches[0].data);
-    }
-  })
+  getSomething('matches', function (data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(data[0].data);
+  });
 };
 
 exports.today = function(req, res){
-  var mongoDb = db.getDbInstance();
-  mongoDb.world_cup_data.find({key: 'today'}, function(err, today){
-    if ( err || !today) {
-      console.log('Likely no connection');
-    }
-    else {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(today[0].data);
-    }
-  })
+  getSomething('today', function (data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(data[0].data);
+  });
 };
